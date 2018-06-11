ff2652d2b980cf801650700b6e8a6a543c634a02
rishank1 <39948995+rishank1@users.noreply.github.com>
2018-06-11 16:02:04 +0530

    Update addResumes.js

--- src/Components/addResumes.js
+++ src/Components/addResumes.js
@@ -1,32 +1,41 @@
 import React from 'react'
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import axios from 'axios'
+import { Button, Form, FormGroup, Label,FormText, Input, Badge } from 'reactstrap';
 
 export class AddResumes extends React.Component{
+	
+	
 		render(){
 				return (
 				<div>
-					<Form>
+				<div>
+					<h2><Badge color="info">Add A New Resume</Badge></h2>
+				</div>
+				<br/>
+				<div>
+					<Form id='uploadForm' action='/first1/upload'  method='post' encType="multipart/form-data">
 						<FormGroup>
 						  <Label for="exampleEmail">Name</Label>
-						  <Input type="text" name="name" id="exampleEmail" placeholder="Name" />
+						  <Input type="text" name="name" id="exampleName" ref={(ref) => { this.name = ref; }} placeholder="Name" />
 						</FormGroup>
 						<FormGroup>
 						  <Label for="exampleEmail">Email</Label>
-						  <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+						  <Input type="email" name="email" id="exampleEmail" ref={(ref) => { this.email = ref; }} placeholder="Email" />
 						</FormGroup>
 						<FormGroup>
 						  <Label for="exampleText">Skills</Label>
-						  <Input type="text" name="email" id="exampleText" placeholder="Skills" />
+						  <Input type="text" name="skills" id="exampleText" ref={(ref) => { this.skills = ref; }} placeholder="Skills" />
 						</FormGroup>
 						<FormGroup>
 						  <Label for="exampleFile">File</Label>
-						  <Input type="file" name="file" id="exampleFile" />
+						  <Input type="file" name="file" ref={(ref) => { this.uploadInput = ref; }} id="exampleFile" />
 						  <FormText color="muted">
 							Upload The Resume in PDF Format only
 						  </FormText>
 						</FormGroup>
-						<Button>Submit</Button>
+						<Button type="submit" >Submit</Button>
 					</Form>
+				</div>
 				</div>
 		);
 		}
